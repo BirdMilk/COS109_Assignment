7bd47effc298797ff5ed319469d028b9df570b58
BirdMilk <oenibe@gmail.com>
2025-01-16 07:42:10 +0100

    all done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Simple form submission message
 document.getElementById("contactForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    alert("Thank you for your message! I'll get back to you soon.");
+    alert("Please click on the paper plane to send.");
     document.getElementById("contactForm").reset();
 });
 
